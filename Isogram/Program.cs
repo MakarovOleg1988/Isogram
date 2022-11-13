349c5aba3538e7cdf8e20b2204efe5e22208ee1d
MakarovOleg1988 <makarov.oleg1988@gmail.com>
2022-11-13 19:16:18 +0530

    Добавьте файлы проекта.

---
+++ Isogram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Isogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _isogram = Console.ReadLine();
+            int _isogramtrue = 0;
+
+            foreach (char item in _isogram)
+            {
+                if (_isogram.Count(p => p == item) > 1) _isogramtrue++;
+            }
+
+            if (_isogramtrue > 0) Console.WriteLine("false");
+            else Console.WriteLine("true");
+        }
+    }
+}
